a43c20c4605893df36f3902f5726fb5cb1b201a5
andresgomes <andre_colossus@yahoo.com.br>
2021-09-01 18:30:08 -0300

    Tentando subir no heroku

--- src/main/java/br/com/videos/controller/AutenticationController.java
+++ src/main/java/br/com/videos/controller/AutenticationController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping(params = {"/auth","/")
+@RequestMapping(value = {"/auth","/")
 public class AutenticationController {
 
     @Autowired
