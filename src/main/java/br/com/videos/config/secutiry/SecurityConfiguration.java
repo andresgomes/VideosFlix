40e020480b9f861b0df81d35980e468b76ede772
andresgomes <andre_colossus@yahoo.com.br>
2021-09-02 15:54:11 -0300

    Tentando subir no heroku

--- src/main/java/br/com/videos/config/secutiry/SecurityConfiguration.java
+++ src/main/java/br/com/videos/config/secutiry/SecurityConfiguration.java
@@ -46,6 +46,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/videos/free").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .antMatchers(HttpMethod.POST, "/").permitAll()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
